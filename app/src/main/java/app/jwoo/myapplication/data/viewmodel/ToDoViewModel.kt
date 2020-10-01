81a2a93ef68918ecbd2542b35ed0e1786036766a
John Woo <jdwoo.88@yahoo.com>
2020-10-01 23:33:10 +0800

    ViewModel

---
+++ app/src/main/java/app/jwoo/myapplication/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,29 @@
+package app.jwoo.myapplication.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import app.jwoo.myapplication.data.ToDoDatabase
+import app.jwoo.myapplication.data.models.ToDoData
+import app.jwoo.myapplication.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application) : AndroidViewModel(application) {
+    private val toDoDao = ToDoDatabase.getDatabase(application).todoDao()
+    private val repository: ToDoRepository
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+}
