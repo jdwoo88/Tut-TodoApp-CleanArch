12673ec066bdf790824728e639280fba34d5f4bb
John Woo <jdwoo.88@yahoo.com>
2020-10-01 21:37:49 +0800

    List Fragment Menu

--- app/src/main/java/app/jwoo/myapplication/fragments/list/ListFragment.kt
+++ app/src/main/java/app/jwoo/myapplication/fragments/list/ListFragment.kt
@@ -1,9 +1,7 @@
 package app.jwoo.myapplication.fragments.list
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import app.jwoo.myapplication.R
@@ -26,7 +24,13 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        // Set Menu
+        setHasOptionsMenu(true)
+
         return view
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
 }
