adbf2ac27d2b392b00740db9f0a1fe70a89007fa
John Woo <jdwoo.88@yahoo.com>
2020-10-01 23:02:04 +0800

    Room Type Converter

---
+++ app/src/main/java/app/jwoo/myapplication/data/PriorityConverter.kt
@@ -0,0 +1,16 @@
+package app.jwoo.myapplication.data
+
+import androidx.room.TypeConverter
+
+class PriorityConverter {
+
+    @TypeConverter
+    fun fromPriority(priority: Priority): String {
+        return priority.name
+    }
+
+    @TypeConverter
+    fun toPriority(priority: String): Priority {
+        return Priority.valueOf(priority)
+    }
+}
